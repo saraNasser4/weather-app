82496a050e50fde0a5fc7fb42d163acf8a76e4f1
saraNasser4 <saransser6@gmail.com>
2024-12-06 15:28:17 +0200

    initial

---
+++ js/text.js
@@ -0,0 +1,31 @@
+const apiKey = "cb700e3ddc1b0a7000fddf60f014ef45";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+
+const input = document.querySelector(".search input");
+const btn = document.querySelector(".search button");
+const weatherIcon = document.querySelector(".weather-icon")
+
+async function checkWeather(cityName) {
+    const response =await fetch(apiUrl + cityName +`&appid=${apiKey}`);
+    if (response.status == 404) {
+        document.querySelector(".error").style.display = "block";
+        document.querySelector(".weather").style.display = "none";
+    } else {
+        var data = await response.json();
+        document.querySelector(".city").innerHTML = data.name;
+        document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + `°C`;
+        document.querySelector(".humidity").innerHTML = data.main.humidity + `%`;
+        document.querySelector(".wind").innerHTML = data.wind.speed + ` km/h`;   
+        
+        weatherIcon.src = `img/${data.weather[0].main.toLowerCase()}.png`
+        
+        document.querySelector(".weather").style.display = "block";
+        document.querySelector(".error").style.display = "none";
+    }
+}
+btn.addEventListener("click", () => {
+    checkWeather(input.value);
+})
+
+
+
